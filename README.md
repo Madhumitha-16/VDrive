c3c979db8deea9e3ff517b0e33320b57b42473e9
Madhumitha-16 <madhu.br03@gmail.com>
2024-05-19 16:26:06 +0530

    new backend config

--- README.md
+++
@@ -1,4 +0,0 @@
-# VDrive
-Netlify link : https://vdrive-drive4u.netlify.app/
-
-Demo Video: https://drive.google.com/file/d/1Tufrj6g5ozW4aRZiPGtPBB-_Ot9nM6Ie/view?ts=63411bfb
--- src/app/bookings/bookings.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.css']
-})
-export class BookingsComponent {
-
-}
